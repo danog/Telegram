8d1878fee6e4c40fbee76eff23f9c01e690c77fb
Daniil Gentili <daniil.gentili.dg@gmail.com>
2016-09-23 23:54:39 +0200

    Applied some fixes

---
+++ config.h
@@ -0,0 +1,3 @@
+// Just to make it work, to be replaced with actual values to be used in a specific client
+#define SETUP_API_ID(apiId) apiId = 25628;
+#define SETUP_API_HASH(apiHash) apiHash = @"1fe17cda7d355166cdaa71f04122873c";
